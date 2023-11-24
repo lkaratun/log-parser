13e63f7b5b322dc071068b02d31a5ebe906eefd8
Lev Karatun <lev@setsail.co>
2023-11-24 13:44:21 -0800

    Fix chunk size

--- search.js
+++ search.js
@@ -2,16 +2,14 @@ import fs from 'node:fs/promises';
 import { read, open } from 'fs';
 import util from 'util';
 
-async function getChunk(file, end, chunkSize) {
-  const fileReadResult = await util.promisify(read)(file, Buffer.alloc(chunkSize), 0, chunkSize, end - chunkSize);
-  return fileReadResult.buffer;
-}
+const DEFAULT_CHUNK_SIZE = 16 * 1024; //16kB
 
 export async function lastNLines(fileName, n, searchTerm) {
   const file = await util.promisify(open)(fileName, 'r');
   const result = [];
   let { size: fileSize } = await fs.stat(fileName);
-  const chunkSize = Math.min(25, fileSize);
+  const chunkSize = Math.min(DEFAULT_CHUNK_SIZE, fileSize);
+
   let currentPosition = fileSize;
   while (result.length < n && currentPosition > 0) {
     const chunk = await getChunk(file, currentPosition, chunkSize);
@@ -21,11 +19,27 @@ export async function lastNLines(fileName, n, searchTerm) {
     const matchingLines = searchTerm ? lines.filter(line => line.includes(searchTerm)) : lines;
     result.unshift(...matchingLines);
 
-    const firstLineEndPositon = chunk.indexOf('\n');
-    const partialFirstLine = chunk.slice(0, firstLineEndPositon);
-    const partialFirstLineSize = Buffer.byteLength(partialFirstLine);
-
-    currentPosition -= chunkSize - partialFirstLineSize;
+    const partialLineSize = getPartialLineSize(chunk);
+    currentPosition -= chunkSize - partialLineSize;
   }
   return result.slice(result.length - n);
+}
+
+async function getChunk(file, end, chunkSize) {
+  // If chunk is bigger than size of data to be read, only read existing data
+  const chunkSizeAdjusted = Math.min(chunkSize, end);
+  const fileReadResult = await util.promisify(read)(
+    file,
+    Buffer.alloc(chunkSizeAdjusted),
+    0,
+    chunkSizeAdjusted,
+    end - chunkSizeAdjusted
+  );
+  return fileReadResult.buffer;
+}
+
+function getPartialLineSize(chunk, lineDelimiter = '\n') {
+  const firstLineEndPositon = chunk.indexOf(lineDelimiter);
+  const partialFirstLine = chunk.slice(0, firstLineEndPositon);
+  return Buffer.byteLength(partialFirstLine);
 }
