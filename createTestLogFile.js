cb20555e3f31129ade3d624168c88181a6e2b1ce
Lev Karatun <lev@setsail.co>
2023-11-24 13:18:09 -0800

    Rename script

--- createTestLogFile.js
+++ createTestLogFile.js
@@ -1,12 +1,12 @@
 import fs from 'node:fs/promises';
 import Stream from 'stream';
 
-const largeLog = await fs.open('./log_large', 'w');
+const largeLog = await fs.open('./log', 'w');
 
 let i = 0;
 const readStream = new Stream.Readable({
   read(size) {
-    if (i < 1e8) this.push(`Line ${i++}\n`);
+    if (i < 1e3) this.push(`Line ${i++}\n`);
     else return null;
   }
 });
