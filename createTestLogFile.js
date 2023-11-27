270ed595773ee78fdbc768607ef085d52370f335
Lev Karatun <lev@setsail.co>
2023-11-26 16:05:19 -0800

    Update file generation script

--- createTestLogFile.js
+++ createTestLogFile.js
@@ -1,20 +1,24 @@
 import fs from 'node:fs/promises';
 import Stream from 'stream';
 
-const largeLog = await fs.open('./log', 'w');
+async function run(lineCountPower = 3) {
+  const largeLog = await fs.open(`/var/log/cribl_test_log_${lineCountPower}`, 'w');
 
-let i = 0;
-const readStream = new Stream.Readable({
-  read(size) {
-    if (i < 1e3) this.push(`Line ${i++}\n`);
-    else return null;
-  }
-});
+  let i = 0;
+  const readStream = new Stream.Readable({
+    read(size) {
+      if (i < Math.pow(10, lineCountPower)) this.push(`Line ${i++}\n`);
+      else return null;
+    }
+  });
 
-const writeStream = largeLog.createWriteStream();
+  const writeStream = largeLog.createWriteStream();
 
-readStream.on('error', err => {
-  console.trace(err);
-});
+  readStream.on('error', err => {
+    console.trace(err);
+  });
 
-readStream.pipe(writeStream);
+  readStream.pipe(writeStream);
+}
+
+await run(process.argv[2]);
